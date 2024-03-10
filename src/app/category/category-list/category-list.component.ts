38577fc5d90a1164df4b8f5c0c1acb876cc8a7eb
tschoul <joel.baeni@lernen.siw.swiss>
2024-03-10 14:02:26 +0100

    Added "Kategorie anzeigen" according to Notion Tutorial

--- src/app/category/category-list/category-list.component.ts
+++ src/app/category/category-list/category-list.component.ts
@@ -1,19 +1,70 @@
 import { Component } from '@angular/core';
 import { CategoryModalComponent } from '../category-modal/category-modal.component';
-import { ModalController } from '@ionic/angular';
-import { Category } from '../../shared/domain';
+import {InfiniteScrollCustomEvent, ModalController, RefresherCustomEvent} from '@ionic/angular';
+import { Category, CategoryCriteria } from '../../shared/domain';
+import {CategoryService} from "../category.service";
+import {ToastService} from "../../shared/service/toast.service";
+import {finalize} from "rxjs";
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
 })
 export class CategoryListComponent {
-  constructor(private readonly modalCtrl: ModalController) {}
 
-  async openModal(category?: Category): Promise<void> {
-    const modal = await this.modalCtrl.create({ component: CategoryModalComponent });
-    modal.present();
-    const { role } = await modal.onWillDismiss();
-    console.log('role', role);
+  constructor(
+    private readonly modalCtrl: ModalController,
+    private readonly categoryService: CategoryService,
+    private readonly toastService: ToastService
+) {}
+
+async openModal(category?: Category): Promise<void> {
+  const modal = await this.modalCtrl.create({ component: CategoryModalComponent });
+  modal.present();
+  const { role } = await modal.onWillDismiss();
+  if (role === 'refresh') this.reloadCategories();
+}
+  categories: Category[] | null = null;
+  readonly initialSort = 'name,asc';
+  lastPageReached = false;
+  loading = false;
+  searchCriteria: CategoryCriteria = { page: 0, size: 25, sort: this.initialSort };
+
+  //Kategorie laden Funktion
+private loadCategories(next: () => void = () => {}): void {
+  if (!this.searchCriteria.name) delete this.searchCriteria.name;
+this.loading = true;
+this.categoryService
+  .getCategories(this.searchCriteria)
+  .pipe(
+    finalize(() => {
+      this.loading = false;
+      next();
+    }),
+  )
+  .subscribe({
+    next: (categories) => {
+      if (this.searchCriteria.page === 0 || !this.categories) this.categories = [];
+      this.categories.push(...categories.content);
+      this.lastPageReached = categories.last;
+    },
+    error: (error) => this.toastService.displayErrorToast('Could not load categories', error),
+  });
+}
+
+// Kategorien laden
+ionViewDidEnter(): void {
+  this.loadCategories();
+}
+
+// Kategorie reloaden
+  reloadCategories($event?: any): void {
+    this.searchCriteria.page = 0;
+    this.loadCategories(() => ($event ? ($event as RefresherCustomEvent).target.complete() : {}));
+  }
+  //nächste Page laden
+  loadNextCategoryPage($event: any) {
+    this.searchCriteria.page++;
+    this.loadCategories(() => ($event as InfiniteScrollCustomEvent).target.complete());
   }
 }
