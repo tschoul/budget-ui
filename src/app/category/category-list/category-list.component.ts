b82dc14cf6a5e81a9727324d6bde3c6f537f2f1e
tschoul <joel.baeni@lernen.siw.swiss>
2024-03-10 14:34:45 +0100

    Added "Kategorie bearbeiten & löschen" according to Notion Tutorial

--- src/app/category/category-list/category-list.component.ts
+++ src/app/category/category-list/category-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CategoryModalComponent } from '../category-modal/category-modal.component';
 import {InfiniteScrollCustomEvent, ModalController, RefresherCustomEvent} from '@ionic/angular';
-import { Category, CategoryCriteria } from '../../shared/domain';
+import {Category, CategoryCriteria, SortOption} from '../../shared/domain';
 import {CategoryService} from "../category.service";
 import {ToastService} from "../../shared/service/toast.service";
-import {finalize} from "rxjs";
+import {debounce, finalize, interval, Subscription} from "rxjs";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-category-list',
@@ -15,15 +16,28 @@ export class CategoryListComponent {
   constructor(
     private readonly modalCtrl: ModalController,
     private readonly categoryService: CategoryService,
-    private readonly toastService: ToastService
-) {}
+    private readonly toastService: ToastService,
+    private readonly formBuilder: FormBuilder
+) {
+    this.searchForm = this.formBuilder.group({ name: [], sort: [this.initialSort] });
+    this.searchFormSubscription = this.searchForm.valueChanges
+      .pipe(debounce((value) => interval(value.name?.length ? 400 : 0)))
+      .subscribe((value) => {
+        this.searchCriteria = { ...this.searchCriteria, ...value, page: 0 };
+        this.loadCategories();
+      });
+  }
 
-async openModal(category?: Category): Promise<void> {
-  const modal = await this.modalCtrl.create({ component: CategoryModalComponent });
-  modal.present();
-  const { role } = await modal.onWillDismiss();
-  if (role === 'refresh') this.reloadCategories();
-}
+
+  async openModal(category?: Category): Promise<void> {
+    const modal = await this.modalCtrl.create({
+      component: CategoryModalComponent,
+      componentProps: { category: category ? { ...category } : {} },
+    });
+    modal.present();
+    const { role } = await modal.onWillDismiss();
+    if (role === 'refresh') this.reloadCategories();
+  }
   categories: Category[] | null = null;
   readonly initialSort = 'name,asc';
   lastPageReached = false;
@@ -67,4 +81,16 @@ ionViewDidEnter(): void {
     this.searchCriteria.page++;
     this.loadCategories(() => ($event as InfiniteScrollCustomEvent).target.complete());
   }
+
+  ionViewDidLeave(): void {
+    this.searchFormSubscription.unsubscribe();
+  }
+  readonly searchForm: FormGroup;
+  readonly sortOptions: SortOption[] = [
+    { label: 'Created at (newest first)', value: 'createdAt,desc' },
+    { label: 'Created at (oldest first)', value: 'createdAt,asc' },
+    { label: 'Name (A-Z)', value: 'name,asc' },
+    { label: 'Name (Z-A)', value: 'name,desc' },
+  ];
+  private readonly searchFormSubscription: Subscription;
 }
--- src/app/category/category-modal/category-modal.component.ts
+++ src/app/category/category-modal/category-modal.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { ActionSheetService } from '../../shared/service/action-sheet.service';
-import { filter, finalize, from } from 'rxjs';
+import {filter, finalize, from, mergeMap, tap} from 'rxjs';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CategoryService } from '../category.service';
 import { ToastService } from '../../shared/service/toast.service';
@@ -14,6 +14,7 @@ import { Category } from 'src/app/shared/domain';
 export class CategoryModalComponent {
   readonly categoryForm: FormGroup;
   submitting = false;
+  category: Category = {} as Category;
   constructor(
     private readonly actionSheetService: ActionSheetService,
     private readonly categoryService: CategoryService,
@@ -21,6 +22,7 @@ export class CategoryModalComponent {
     private readonly modalCtrl: ModalController,
     private readonly toastService: ToastService
   ) { this.categoryForm = this.formBuilder.group({
+    id: [], // hidden
     name: ['', [Validators.required, Validators.maxLength(40)]],
   });
   }
@@ -45,11 +47,21 @@ export class CategoryModalComponent {
 
   delete(): void {
     from(this.actionSheetService.showDeletionConfirmation('Are you sure you want to delete this category?'))
-      .pipe(filter((action) => action === 'delete'))
+      .pipe(
+        filter((action) => action === 'delete'),
+        tap(() => (this.submitting = true)),
+        mergeMap(() => this.categoryService.deleteCategory(this.category.id!)),
+        finalize(() => (this.submitting = false)),
+      )
       .subscribe({
         next: () => {
-          this.modalCtrl.dismiss(null, 'delete');
+          this.toastService.displaySuccessToast('Category deleted');
+          this.modalCtrl.dismiss(null, 'refresh');
         },
+        error: (error) => this.toastService.displayErrorToast('Could not delete category', error),
       });
+  }
+  ionViewWillEnter(): void {
+    this.categoryForm.patchValue(this.category);
   }
 }
