b761c80bedf66cec01381275542f465dc2800416
tschoul <joel.baeni@lernen.siw.swiss>
2024-03-31 15:34:21 +0200

    Ausgabe bearbeiten & löschen abgeschlossen

--- src/app/expense/expense-modal/expense-modal.component.ts
+++ src/app/expense/expense-modal/expense-modal.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {ModalController} from '@ionic/angular';
-import {filter, finalize, from} from 'rxjs';
+import {filter, finalize, from, mergeMap, tap} from 'rxjs';
 import {CategoryModalComponent} from "../../category/category-modal/category-modal.component";
 import {ActionSheetService} from '../../shared/service/action-sheet.service';
 import {ExpenseService} from "../expense.service";
@@ -60,8 +60,19 @@ export class ExpenseModalComponent {
 
   delete(): void {
     from(this.actionSheetService.showDeletionConfirmation('Are you sure you want to delete this expense?'))
-      .pipe(filter((action) => action === 'delete'))
-      .subscribe(() => this.modalCtrl.dismiss(null, 'delete'));
+      .pipe(
+        filter((action) => action === 'delete'),
+        tap(() => (this.submitting = true)),
+        mergeMap(() => this.expenseService.deleteExpense(this.expense.id!)),
+        finalize(() => (this.submitting = false)),
+      )
+      .subscribe({
+        next: () => {
+          this.toastService.displaySuccessToast('Expense deleted');
+          this.modalCtrl.dismiss(null, 'refresh');
+        },
+        error: (error) => this.toastService.displayErrorToast('Could not delete expense', error),
+      });
   }
 
   async showCategoryModal(): Promise<void> {
@@ -78,5 +89,8 @@ export class ExpenseModalComponent {
   }
   ionViewWillEnter(): void {
     this.loadAllCategories();
+    const { id, amount, category, date, name } = this.expense;
+    if (category) this.categories.push(category);
+    if (id) this.expenseForm.patchValue({ id, amount, categoryId: category?.id, date, name });
   }
 }
